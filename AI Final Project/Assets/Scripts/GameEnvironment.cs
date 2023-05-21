455c8181c1b30f3b27c289dd24e4f1349ebcd400
MuffinWithBananaJam <87189263+MuffinWithBananaJam@users.noreply.github.com>
2023-05-21 06:03:50 +0300

    more stuff

---
+++ AI Final Project/Assets/Scripts/GameEnvironment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints
+    {
+        get { return checkpoints; }
+    }
+
+    public static GameEnvironment singleton
+    {
+        get
+        {
+            if (instance==null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+
+            return instance;
+        }
+    }
+}
